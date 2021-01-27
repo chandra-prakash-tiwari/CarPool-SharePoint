2a07d1f071af984b70d4b8fb1058543de7680034
chandra prakash tiwari <tiwarivas88@gmail.com>
2021-01-27 20:49:33 +0530

    commit

--- src/App/LoginLayout.tsx
+++ src/App/LoginLayout.tsx
@@ -1,19 +1,17 @@
-﻿import * as React from 'react';
-import { Grid } from '@material-ui/core';
-import Login from '../components/Anonymus/Login';
-import Text from '../components/Anonymus/Text'
+﻿import * as React from "react";
+import { Grid } from "@material-ui/core";
+import Login from "../components/Anonymus/Login";
+import Text from "../components/Anonymus/Text";
 
-export default class LoginLayout extends React.Component{
-    render() {
-        return (
-            <Grid container>
-                <Grid item xs={false} sm={4} md={8} className='page image'>
-                    <Text/>
-                </Grid>
-                <Grid item xs={12} sm={8} md={4} className='page image'>
-                    <Login />
-                </Grid>
-            </Grid>
-            )
-    }
+export default class LoginLayout extends React.Component {
+  render() {
+    return (
+      <Grid container>
+        <Grid item xs={false} sm={4} md={8} className="page image">
+          <Text />
+        </Grid>
+        <Login />
+      </Grid>
+    );
+  }
 }
