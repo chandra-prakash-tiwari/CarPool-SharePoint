2fcb8c2c5ce0c9f5a4f9cdbda5bda27ce9ebfe80
chandra prakash tiwari <tiwarivas88@gmail.com>
2021-01-29 21:47:49 +0530

    commit

--- src/App/LoginLayout.tsx
+++ src/App/LoginLayout.tsx
@@ -4,10 +4,12 @@ import Login from "../components/Anonymus/Login";
 import Text from "../components/Anonymus/Text";
 
 export default class LoginLayout extends React.Component {
-  render() { 
+  render() {
     return (
       <Grid container component="main">
-        <Grid item xs={false} sm={6} md={8} className="page image"></Grid>
+        <Grid item xs={false} sm={6} md={8} className="page image">
+          <Text />
+        </Grid>
         <Grid item xs={12} sm={6} md={4} component={Paper} elevation={6} square>
           <Login />
         </Grid>
