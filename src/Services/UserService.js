8c9acfe8139be71592aa09800c374d15c3ed899b
chandra prakash tiwari <tiwarivas88@gmail.com>
2021-01-29 21:39:31 +0530

    commit

--- src/Services/UserService.js
+++ src/Services/UserService.js
@@ -1,223 +1,238 @@
-﻿
-const getCurrentUser = JSON.parse(localStorage.getItem('currentUser')); 
+﻿const getCurrentUser = JSON.parse(localStorage.getItem("currentUser"));
 
 export const UserService = {
-    getUser,
-    login,
-    logout,
-    sessionExpired,
-    addNewUser,
-    validEmail,
-    validUserName,
-    getToken,
-    getRequestDigest,
-    currentUser: getCurrentUser
-}
+  getUser,
+  login,
+  logout,
+  sessionExpired,
+  addNewUser,
+  validEmail,
+  validUserName,
+  getToken,
+  getRequestDigest,
+  currentUser: getCurrentUser,
+};
 
 function logout() {
-    localStorage.removeItem('currentUser');
+  localStorage.removeItem("currentUser");
 }
 
-function validUserName(userName){
-    var token=localStorage.getItem('token');
-    var url="https://chandraprakashtiwariv.sharepoint.com/sites/carpool/_api/lists/getbytitle('user')/items?";
-    var query=`$filter=UserName eq '${btoa(userName)}'`;
+function validUserName(userName) {
+  var token = localStorage.getItem("token");
+  var url =
+    "https://chandraprakashtiwariv.sharepoint.com/sites/carpool/_api/lists/getbytitle('user')/items?";
+  var query = `$filter=UserName eq '${btoa(userName)}'`;
 
-    return fetch(url+query, {
-        headers: { 
-            Accept:'application/json;odata=nometadata',
-            'Content-Type': 'application/json' ,
-            Authorization:`Bearer ${token}`
-        },
-    }).then(async response => {
-            if (response.status === 200) {
-                const data = await response.json();
-                if(data.value.length === 1)
-                {
-                    return true;
-                }
-                return false;
-            }
-            return false;
-        }).catch(error => {
-            return error;
-        })
+  return fetch(url + query, {
+    headers: {
+      Accept: "application/json;odata=nometadata",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then(async (response) => {
+      if (response.status === 200) {
+        const data = await response.json();
+        if (data.value.length === 1) {
+          return true;
+        }
+        return false;
+      }
+      return false;
+    })
+    .catch((error) => {
+      return error;
+    });
 }
 
-function validEmail(email){
-    var token=localStorage.getItem('token');
-    var url="https://chandraprakashtiwariv.sharepoint.com/sites/carpool/_api/lists/getbytitle('user')/items?";
-    var query=`$filter=Email eq '${btoa(email)}'`;
+function validEmail(email) {
+  var token = localStorage.getItem("token");
+  var url =
+    "https://chandraprakashtiwariv.sharepoint.com/sites/carpool/_api/lists/getbytitle('user')/items?";
+  var query = `$filter=Email eq '${btoa(email)}'`;
 
-    return fetch(url+query, {
-        headers: { 
-            Accept:'application/json;odata=nometadata',
-            'Content-Type': 'application/json' ,
-            Authorization:`Bearer ${token}`
-        },
-    }).then(async response => {
-            if (response.status === 200) {
-                const data = await response.json();
-                if(data.value.length === 1)
-                {
-                    return true;
-                }
-            }
-            return false;
-        }).catch(error => {
-            return error;
-        })
+  return fetch(url + query, {
+    headers: {
+      Accept: "application/json;odata=nometadata",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then(async (response) => {
+      if (response.status === 200) {
+        const data = await response.json();
+        if (data.value.length === 1) {
+          return true;
+        }
+      }
+      return false;
+    })
+    .catch((error) => {
+      return error;
+    });
 }
 
 function sessionExpired() {
-    localStorage.clear();
-    window.location.pathname = '/home';
+  localStorage.clear();
+  window.location.pathname = "/home";
 }
 
-function getUser(email){
-    var token=localStorage.getItem('token');
-    var url="https://chandraprakashtiwariv.sharepoint.com/sites/carpool/_api/lists/getbytitle('user')/items?";
-    var query=`$filter=Email eq '${btoa(email)}'`;
+function getUser(email) {
+  var token = localStorage.getItem("token");
+  var url =
+    "https://chandraprakashtiwariv.sharepoint.com/sites/carpool/_api/lists/getbytitle('user')/items?";
+  var query = `$filter=Email eq '${btoa(email)}'`;
 
-    return fetch(url+query, {
-        headers: { 
-            Accept:'application/json;odata=nometadata',
-            'Content-Type': 'application/json' ,
-            Authorization:`Bearer ${token}`
-        },
-    }).then(async response => {
-            if (response.status === 200) {
-                const data = await response.json();
-                if(data.value.length === 1)
-                {
-                    var rec={
-                        name:atob(data.value[0].Title),
-                        email:atob(data.value[0].Email),
-                        address:atob(data.value[0].Address),
-                        mobile:atob(data.value[0].Mobile),
-                        userName:atob(data.value[0].UserName),
-                        drivingLicence:atob(data.value[0].DrivingLicence),
-                        id:atob(data.value[0].ID)
-                    }
-                    return rec;
-                }
-            }
-            return Promise.reject();
-        }).catch(error => {
-            return error;
-        })
-}
-
-async function getRequestDigest(){
-    var token=localStorage.getItem('token');
-
-    return fetch("https://chandraprakashtiwariv.sharepoint.com/sites/carpool/_api/contextinfo",{
-        method:"POST",
-		headers:{
-            Accept:"application/json;odata=nometadata",
-            Authorization:`Bearer ${token}`
-        },
-    }).then(async res=>{
-        localStorage.setItem('digest',JSON.stringify(await res.json()));
+  return fetch(url + query, {
+    headers: {
+      Accept: "application/json;odata=nometadata",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then(async (response) => {
+      if (response.status === 200) {
+        const data = await response.json();
+        if (data.value.length === 1) {
+          var rec = {
+            name: atob(data.value[0].Title),
+            email: atob(data.value[0].Email),
+            address: atob(data.value[0].Address),
+            mobile: atob(data.value[0].Mobile),
+            userName: atob(data.value[0].UserName),
+            drivingLicence: atob(data.value[0].DrivingLicence),
+            id: data.value[0].ID,
+          };
+          return rec;
+        }
+      }
+      return Promise.reject();
     })
+    .catch((error) => {
+      return error;
+    });
 }
 
-async function getToken(){
-    return fetch('https://cors-anywhere.herokuapp.com/https://accounts.accesscontrol.windows.net/4e847bc8-615a-4c91-a8ca-8cff7106481e/tokens/OAuth/2',{
-        method:"POST",
-        headers:{
-            Accept:"application/json;odata=verbose",
-            "Content-Type":"application/x-www-form-urlencoded"
-        },
+async function getRequestDigest() {
+  var token = localStorage.getItem("token");
 
-        body : `grant_type=client_credentials&client_id=766f911b-5b3c-4ea4-a67d-564080155d94@4e847bc8-615a-4c91-a8ca-8cff7106481e&client_secret=UJQhDYryjeR36gTMq2Dkc3zQ39e9CPaPk8xVk9gFPw8=&resource=00000003-0000-0ff1-ce00-000000000000/chandraprakashtiwariv.sharepoint.com@4e847bc8-615a-4c91-a8ca-8cff7106481e`
-    }).then(a => a.json()).then(a => localStorage.setItem('token',a.access_token)).catch(e => console.log(e))
-    
+  return fetch(
+    "https://chandraprakashtiwariv.sharepoint.com/sites/carpool/_api/contextinfo",
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json;odata=nometadata",
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  ).then(async (res) => {
+    localStorage.setItem("digest", JSON.stringify(await res.json()));
+  });
 }
 
-function addNewUser(userData){
-   
-    getToken();
-    getRequestDigest();
-    var digest=localStorage.getItem('digest');
+async function getToken() {
+  return fetch(
+    "https://cors-anywhere.herokuapp.com/https://accounts.accesscontrol.windows.net/4e847bc8-615a-4c91-a8ca-8cff7106481e/tokens/OAuth/2",
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json;odata=verbose",
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
 
-    var token=localStorage.getItem('token');
-    var data1 = {
-        Title:btoa(userData.name),
-        Mobile: btoa(userData.mobile),
-        Address: btoa(userData.address),
-        Email: btoa(userData.email),
-        Password: btoa(userData.password),
-        UserName: btoa(userData.userName),
-        DrivingLicence: btoa(userData.drivingLicence)
+      body: `grant_type=client_credentials&client_id=766f911b-5b3c-4ea4-a67d-564080155d94@4e847bc8-615a-4c91-a8ca-8cff7106481e&client_secret=UJQhDYryjeR36gTMq2Dkc3zQ39e9CPaPk8xVk9gFPw8=&resource=00000003-0000-0ff1-ce00-000000000000/chandraprakashtiwariv.sharepoint.com@4e847bc8-615a-4c91-a8ca-8cff7106481e`,
+    }
+  )
+    .then((a) => a.json())
+    .then((a) => localStorage.setItem("token", a.access_token))
+    .catch((e) => console.log(e));
+}
+
+function addNewUser(userData) {
+  getToken();
+  getRequestDigest();
+  var digest = localStorage.getItem("digest");
+
+  var token = localStorage.getItem("token");
+  var data1 = {
+    Title: btoa(userData.name),
+    Mobile: btoa(userData.mobile),
+    Address: btoa(userData.address),
+    Email: btoa(userData.email),
+    Password: btoa(userData.password),
+    UserName: btoa(userData.userName),
+    DrivingLicence: btoa(userData.drivingLicence),
+  };
+
+  var url =
+    "https://chandraprakashtiwariv.sharepoint.com/sites/carpool/_api/lists/getbytitle('user')/items";
+
+  return fetch(url, {
+    method: "POST",
+    headers: {
+      Accept: "application/json;odata=verbose",
+      "x-RequestDigest": digest.FormDigestValue,
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(data1),
+  }).then(async (r) => {
+    if (r.status === 201) {
+      return Promise.resolve("Ok");
     }
 
-    var url="https://chandraprakashtiwariv.sharepoint.com/sites/carpool/_api/lists/getbytitle('user')/items";
-
-        return fetch(url,{
-			method:"POST",
-			headers:{
-				Accept:"application/json;odata=verbose",
-                "x-RequestDigest":digest.FormDigestValue,
-                "Content-Type":"application/json",
-                Authorization:`Bearer ${token}`
-			},
-			body:JSON.stringify(data1)	
-		}).then(async r=>{
-            if(r.status===201){
-                return Promise.resolve("Ok");
-            }
-
-            return Promise.reject("Reject");
-        });
+    return Promise.reject("Reject");
+  });
 }
 
-function login(loginDetails)
-{
-    getToken();
-    getRequestDigest();
-    var token=localStorage.getItem('token');
+function login(loginDetails) {
+  getToken();
+  getRequestDigest();
+  var token = localStorage.getItem("token");
 
-    var url="https://chandraprakashtiwariv.sharepoint.com/sites/carpool/_api/lists/getbytitle('user')/items?";
-    var query=`$filter=((UserName eq '${btoa(loginDetails.userName)}') or (Email eq '${btoa(loginDetails.userName)}'))and(Password eq '${btoa(loginDetails.password)}')`;
+  var url =
+    "https://chandraprakashtiwariv.sharepoint.com/sites/carpool/_api/lists/getbytitle('user')/items?";
+  var query = `$filter=((UserName eq '${btoa(
+    loginDetails.userName
+  )}') or (Email eq '${btoa(loginDetails.userName)}'))and(Password eq '${btoa(
+    loginDetails.password
+  )}')`;
 
-    return fetch(url+query, {
-        headers: { 
-            Accept:'application/json;odata=nometadata',
-            'Content-Type': 'application/json' ,
-            Authorization:`Bearer ${token}`
-        },
-    }).then(async response => {
-        if (response.status === 200) {
-            const data = await response.json();
-            if(data.value.length === 1){
-                var rec={
-                    name:atob(data.value[0].Title),
-                    email:atob(data.value[0].Email),
-                    address:atob(data.value[0].Address),
-                    mobile:atob(data.value[0].Mobile),
-                    userName:atob(data.value[0].UserName),
-                    drivingLicence:atob(data.value[0].DrivingLicence),
-                    id:atob(data.value[0].ID)
-                }
-                localStorage.setItem('currentUser', JSON.stringify(rec));
-                return Promise.resolve("ok");
-            }
-            else{
-                return Promise.reject("wrong");
-            }
+  return fetch(url + query, {
+    headers: {
+      Accept: "application/json;odata=nometadata",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then(async (response) => {
+      if (response.status === 200) {
+        const data = await response.json();
+        if (data.value.length === 1) {
+          var rec = {
+            name: atob(data.value[0].Title),
+            email: atob(data.value[0].Email),
+            address: atob(data.value[0].Address),
+            mobile: atob(data.value[0].Mobile),
+            userName: atob(data.value[0].UserName),
+            drivingLicence: atob(data.value[0].DrivingLicence),
+            id: data.value[0].ID,
+          };
+          localStorage.setItem("currentUser", JSON.stringify(rec));
+          return Promise.resolve("ok");
+        } else {
+          return Promise.reject("wrong");
         }
-        else if (response.status === 204) 
-            return Promise.reject("wrong");
+      } else if (response.status === 204) return Promise.reject("wrong");
+      else if (response.status === 500) {
+        return Promise.reject("servererror");
+      }
 
-        else if (response.status === 500) {
-            return Promise.reject("servererror");
-        }
-
-        return Promise.reject();
-    }).catch(error => {
-        return error;
+      return Promise.reject();
     })
+    .catch((error) => {
+      return error;
+    });
 }
 
 export default UserService;
